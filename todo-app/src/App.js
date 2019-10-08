20216a78b83001b99be32a503abc1b23807fd907
Manju KC <kcmanjuthapa@gmail.com>
2019-10-08 16:27:00 -0400

    created todoForm

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import TodoList from "./Component/toDoList"
+import Form from "./Component/TodoForm"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList></TodoList>
+        <TodoList/>
+        <Form/>
       </div>
     );
   }
---
+++ todo-app/src/Component/TodoForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+
+
+    return (
+        <div>
+            <input/>
+        </div>
+    )
+}
+
+export default Form
